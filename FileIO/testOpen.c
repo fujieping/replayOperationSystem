9d6588b432929652302d437781725c37a9d0194c
挡泥板 <1573465644@qq.com>
2024-01-04 16:22:12 +0800

    [修改]验证文件描述符

---
+++ FileIO/testOpen.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+
+int main()
+{
+    int fd1 = open("./hello.txt", O_RDWR | O_CREAT, 0644);
+    if (fd1 == -1)
+    {
+        perror("npen error");
+    }
+    printf("fd1 = %d\n", fd1);
+
+    int fd2 = open("./hello.txt", O_RDWR | O_CREAT, 0644);
+    if (fd2 == -1)
+    {
+        perror("npen error");
+    }
+    printf("fd2 = %d\n", fd2);
+
+    int fd3 = open("./hello.txt", O_RDWR | O_CREAT, 0644);
+    if (fd3 == -1)
+    {
+        perror("npen error");
+    }
+    printf("fd3 = %d\n", fd3);
+
+    int fd4 = open("./hello.txt", O_RDWR | O_CREAT, 0644);
+    if (fd4 == -1)
+    {
+        perror("npen error");
+    }
+    printf("fd4 = %d\n", fd4);
+
+    /* 关闭文件*/
+    close(fd1);
+
+    int fd5 = open("./hello.txt", O_RDWR | O_CREAT, 0644);
+    if (fd5 == -1)
+    {
+        perror("npen error");
+    }
+    printf("fd5 = %d\n", fd5);
+
+    return 0;
+}
